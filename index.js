41ae4b4ff53fb83d6492b81eb2bc4bb7af9fe3e0
sadikhossain69 <sadikhossain433@gmail.com>
2022-07-01 03:47:13 +0600

    server created

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 5000
+
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("Hello Todo App")
+})
+
+app.listen(port, () => {
+    console.log("Your Server Is Running at", port);
+})
