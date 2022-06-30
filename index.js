d7d6e1e8284968d9dc78905adcc2a0982dbf1e5a
sadikhossain69 <sadikhossain433@gmail.com>
2022-07-01 04:02:47 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
-
+require('dotenv').config()
 
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a6fpj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run () {
+    try {
+        await client.connect()
+        console.log("DB Connected");
+    } 
+    
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir)
+
 
 app.get('/', (req, res) => {
     res.send("Hello Todo App")
